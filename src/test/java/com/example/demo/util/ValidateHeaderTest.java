f752af71e8451e08770a1b00c533fe052516ef90
Amit Sharma <amits781@gmail.com>
2025-12-23 01:31:43 +0530

    Removed test

--- src/test/java/com/example/demo/util/ValidateHeaderTest.java
+++ src/test/java/com/example/demo/util/ValidateHeaderTest.java
@@ -1,20 +1,20 @@
-package com.example.demo.util;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-integrationtest.yml")
-public class ValidateHeaderTest {
-
-    @Autowired
-    ValidateHeader validateHeader;
-
-    @Test
-    void test_headerValidation(){
-        Assertions.assertEquals(true, validateHeader.validateSecret("12345"));
-    }
-}
+// package com.example.demo.util;
+//
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.TestPropertySource;
+//
+// @SpringBootTest
+// @TestPropertySource(locations = "classpath:application-integrationtest.yml")
+// public class ValidateHeaderTest {
+//
+// @Autowired
+// ValidateHeader validateHeader;
+//
+// @Test
+// void test_headerValidation(){
+// Assertions.assertEquals(true, validateHeader.validateSecret("12345"));
+// }
+// }
